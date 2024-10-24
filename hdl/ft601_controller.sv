6db2ae002526623edeb159d907184d8f5573d835
mjhershfield <mhershfield@ufl.edu>
2024-10-24 11:01:59 -0400

    Update FT601 controller interface

--- hdl/ft601_controller.sv
+++ hdl/ft601_controller.sv
@@ -1,19 +1,24 @@
 module ft601_controller
     (
+        input logic clk,
+        input logic rst,
+
         // Control signals coming from the FT601
-        input logic tx_full,
-        input logic rx_empty,
-        // Whether valid data is coming from the arbiter
-        input logic tx_valid,
+        input logic usb_tx_full,
+        input logic usb_rx_empty,
+
         // Control signals going to FT601
-        output logic wren,
-        output logic rden,
-        output logic outen,
-        output logic rst,
+        output logic usb_wren_l,
+        output logic usb_rden_l,
+        output logic usb_outen_l,
+        output logic usb_rst_l,
+
         // 0 = input from data bus, 1 = outputting to data bus
-        output logic data_bus_dir,
+        output logic ft601_data_bus_dir,
+        // Whether valid data is coming from the arbiter
+        input logic periph_data_available,
         // Allow peripherals to send data back to USB host
-        output logic allow_tx
+        output logic read_periph_data
     );
 
 endmodule
